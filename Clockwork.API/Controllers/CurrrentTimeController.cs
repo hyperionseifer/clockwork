ddeb7ff0b5e9054b2e8b8dfd4de582408abd7e5b
Seph Reyes <hyperionseifer13@gmail.com>
2019-03-23 13:37:57 +0800

    Initial commits to own repository - Updated API to be more organized it terms of connection settings and class arrangements. - Used DI for the DbContext. - Added bootstrap resource files in the UI project.

---
+++ Clockwork.API/Controllers/CurrrentTimeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Clockwork.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Clockwork.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clockwork.API.Controllers
+{
+    public class CurrentTimeController : Controller
+    {
+      private readonly ClockworkContext _context;
+
+      public CurrentTimeController(ClockworkContext context)
+      {
+        _context = context;
+      }
+
+      // GET api/currenttime
+      [Route("api/[controller]")]
+      [HttpGet]
+      public IActionResult Get()
+      {
+        var utcTime = DateTime.UtcNow;
+        var serverTime = DateTime.Now;
+        var remoteIp = HttpContext.Connection.RemoteIpAddress.ToString();
+
+        var currentTimeDetail = new CurrentTimeQuery
+        {
+            UTCTime = utcTime,
+            ClientIp = remoteIp,
+            Time = serverTime
+        };
+
+        _context.CurrentTimeQueries.Add(currentTimeDetail);
+        _context.SaveChanges();
+
+        return Ok(currentTimeDetail);
+      }
+
+    [Route("api/[controller]/getall")]
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+      var items = await _context.CurrentTimeQueries.OrderByDescending(q => q.CurrentTimeQueryId).ToListAsync();
+      return Ok(items);
+    }
+  }
+}
---
+++ Clockwork.API/Models/CurrentTimeQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Clockwork.API.Models
+{
+    public class CurrentTimeQuery
+    {
+        public int CurrentTimeQueryId { get; set; }
+        public DateTime Time { get; set; }
+        public string ClientIp { get; set; }
+        public DateTime UTCTime { get; set; }
+    }
+}
